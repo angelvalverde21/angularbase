93ea5f6773e4aa20e4fb8a29f3b7079aeafe2fac
ANGEL VALVERDE <angelvalverde21@gmail.com>
2020-04-07 14:10:02 -0500

    proyecto base con login y firebase

---
+++ src/app/pages/registro/registro.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit } from '@angular/core';
+import { UsuarioModel } from '../../models/usuario.model';
+import { NgForm } from '@angular/forms';
+
+import { AuthService } from '../../servicios/auth.service';
+
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-registro',
+  templateUrl: './registro.component.html',
+  styleUrls: ['./registro.component.css']
+})
+export class RegistroComponent implements OnInit {
+
+
+  usuario: UsuarioModel;
+  recordarme: boolean = false;
+
+
+  constructor(
+    private auth: AuthService,
+    private router: Router
+  ) { }
+
+  ngOnInit() { 
+
+    this.usuario = new UsuarioModel();
+
+    //this.usuario.email = "angelvalverde21@gmail.com";
+
+  }
+
+
+
+  onSubmit( form: NgForm){
+
+  if ( form.invalid ) { return; }
+
+  Swal.fire({
+    allowOutsideClick: false,
+    text: 'Espere por favor...'
+  });
+
+  //  console.log(this.usuario); 
+    console.log(form); 
+    this.auth.nuevoUsuario ( this.usuario )
+      .subscribe( data => {
+
+        Swal.close();
+        
+        if( this.recordarme ) {
+          localStorage.setItem('email', this.usuario.email);
+        }
+        
+        this.router.navigateByUrl('/home');
+
+        console.log(data);
+      }, (err) => {
+        console.log(err.error.error.message);
+        Swal.fire({
+          text: 'La cuenta ya existe...'
+        });
+      });
+
+  }
+
+}
---
+++ src/app/servicios/auth.service.ts
@@ -0,0 +1,86 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { UsuarioModel } from '../models/usuario.model';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+    private url = 'https://identitytoolkit.googleapis.com/v1/accounts';
+    private apikey = 'AIzaSyCqcl7IP9sDxuqKMaKBE4JDwu7Es7l2kws';
+ //https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=[API_KEY]
+
+ userToken: string;
+  
+  constructor( private http: HttpClient) {
+  
+    this.lerrToken();
+
+   }
+
+  logout(){
+    localStorage.removeItem('token');
+  }
+
+
+  login( usuario: UsuarioModel ){
+    const authData = {
+      email: usuario.email,
+      password: usuario.password,
+      returnSecureToken: true
+    }
+
+    return this.http.post(`${ this.url }:signInWithPassword?key=${ this.apikey }`,authData)                
+      .pipe(
+        map( resp => {
+          console.log('entreo al map del rxjs');
+          this.guardarToken( resp['idToken'] );
+          return resp; 
+        })
+      );
+
+  }
+
+
+nuevoUsuario( usuario: UsuarioModel ){
+    const authData = {
+      email: usuario.email,
+      password: usuario.password,
+      nombre: usuario.nombre,
+      returnSecureToken: true
+    }
+
+              return this.http.post(`${ this.url }:signUp?key=${ this.apikey }`,authData)
+                .pipe(
+                  map( resp => {
+                    console.log('entreo al map del rxjs');
+                    this.guardarToken( resp['idToken'] );
+                    return resp; 
+                  })
+                );
+
+}
+
+  private guardarToken(idToken: string){
+    this.userToken = idToken;
+    localStorage.setItem('token',idToken);
+  }
+
+  lerrToken(){
+    if(localStorage.getItem('token')){
+      this.userToken = localStorage.getItem('token');
+    }else{
+      this.userToken = '';
+    }
+
+    return this.userToken;
+  }
+  
+
+  estaAutenticado():boolean{
+    return this.userToken.length > 2;
+  }
+
+}
